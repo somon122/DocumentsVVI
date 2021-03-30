1f4b50abcabff3508e26d155526aa38362da3ef5
Md. Somon Hossain <somon12230327@gmail.com>
2021-03-30 14:32:18 +0600

    first commit

---
+++ Documents/VPN DEtact Cose.java
@@ -0,0 +1,19 @@
+//For detecting vpn
+    public boolean vpn() {
+        String iface = "";
+        try {
+            for (NetworkInterface networkInterface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+                if (networkInterface.isUp())
+                    iface = networkInterface.getName();
+                Log.d("DEBUG", "IFACE NAME: " + iface);
+                if ( iface.contains("tun") || iface.contains("ppp") || iface.contains("pptp")) {
+                    return true;
+                }
+            }
+        } catch (SocketException e1) {
+            e1.printStackTrace();
+        }
+
+        return false;
+    }
+    //
